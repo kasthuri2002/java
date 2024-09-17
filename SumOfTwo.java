fd7780074b5d1724cdea939447eebcce09363f69
kasthuri2002 <arthisk12@gmail.com>
2024-09-17 12:00:34 +0530

    Sum of Two Numbers

---
+++ SumOfTwo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SumOfTwo {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter num1:"); 
+    int num1=sc.nextInt();
+    System.out.print("Enter num2:"); 
+    int num2=sc.nextInt();  
+    System.out.println("Sum of two numbers : "+(num1+num2));
+    }
+}
